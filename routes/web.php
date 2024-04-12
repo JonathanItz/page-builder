6dff916c524da0599db8a4a1f49d95115b36f8c4
JonathanItz <jonathanitzen95@gmail.com>
2024-04-12 10:57:51 -0500

    save

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,11 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'welcome')
     ->name('home');
 
+Route::get('/dashboard', function() {
+    return redirect()->route('filament.website.pages.dashboard');
+})
+->name('dashboard');
+
 Route::get('/{unique_id}/{slug}', [PagesController::class, 'show'])
     ->name('page');
 
